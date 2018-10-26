4fab5d8f198eaf1bb8c3e1b5f6a340bc5beeb8c2
aanand-kumar <kumaraanand9903@gmail.com>
2018-10-26 20:25:09 +0530

    something went wrong

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,13 +1,16 @@
 const expect=require("expect")
 
 const request=require("supertest");
+const {ObjectID}=require("mongodb");
 
 var {Todo}=require("./../models/todo");
 var {app}=require("./../server");
 
 var todos=[{
+    _id:new ObjectID(),
     text:"test code 1"
 },{
+    _id:new ObjectID(),
     text:"test cose 2"
 }]
 beforeEach((done)=>{
@@ -45,6 +48,33 @@ describe("GET/todos",()=>{
         request(app)
         .get("/todos")
         .expect(200)
+        .expect((res)=>{
+            expect(res.body.todos.text).toBe(text)
+        })
         .end(done)
+    })
+})
+
+describe("GET/todos/:id",()=>{
+    it("should have valid id",(done)=>{
+        request(app)
+        .get(`/todos/${todos[0]._id.toHexString()}`)
+        .expect(200)
+        .expect((res)=>{
+            expect(res.body.todo.text).toBe(todos[0].text)
+        })
+        .end(done);
+    })
+    it("should have invalid id",(done)=>{
+        request(app)
+        .get(`/todos/${todos[0]._id.toHexString()}1`)
+        .expect(404)
+        .end(done);
+    })
+    it("should have invalid id",(done)=>{
+        request(app)
+        .get(`/todos/123`)
+        .expect(404)
+        .end(done);
     })
 })
