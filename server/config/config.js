7d994721035f4d71e7fea408e70c2b9281a51206
aanand-kumar <kumaraanand9903@gmail.com>
2018-11-02 11:30:21 +0530

    config file adding3

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,11 @@
 var env=process.env.NODE_ENV || "dev";
-
-if (env="dev"){
+console.log("env ******",env)
+if (env=="dev"){
     process.env.PORT=3000;
-    process.env.MONGODB_URI="mongodb://localhost:27017/TodoApp";
-}else if(env="test1"){
+    process.env.MONGODB_URI="mongodb://localhost:27017/TodoApp2";
+}else if(env=="test1"){
     process.env.PORT=3000;
-    process.env.MONGODB_URI="mongodb://localhost:27017/TodoTestApp";
+    process.env.MONGODB_URI="mongodb://localhost:27017/TodoTestApp1";
 }
 else{
     process.env.MONGODB_URI="mongodb://kum:adaanand420@ds139322.mlab.com:39322/mou"
