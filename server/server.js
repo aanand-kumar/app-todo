259ef63ac333c861f480d386e732e904828cde4c
aanand-kumar <kumaraanand9903@gmail.com>
2018-11-02 20:28:54 +0530

    user sign up

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const _=require("lodash")
 var {mongoose}=require("./db/mongoose");
 var {Todo}=require("./models/todo");
 
-var {user}=require("./models/users");
+var {User}=require("./models/users");
 
 var app=express();
 const port=process.env.PORT ;
@@ -85,6 +85,21 @@ app.patch("/todos/:id",(req,res)=>{
         res.send({todo})
     }).catch((e)=>res.status(404).send())
 })
+
+app.post("/users",(req,res)=>{
+    var body =_.pick(req.body,["email","password"]);
+    var user= new User(body)
+    
+    user.save().then((user)=>{
+        return user.genarateAuthToken();
+    }).then((token)=>{
+        
+        res.header("x-auth",token).send(user);
+    }).catch((e)=>{
+        res.status(400).send()
+    })
+
+});
 
 app.listen(port,()=>{
     console.log(`server ${port} is running`)
